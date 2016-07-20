675c5a3a1dfb15761428f151a65640ec981cc414
Rodrigo Dominguez <rorra@rorra.com.ar>
2016-07-20 00:38:21 -0400

    Chapter 9 done

--- web/router.ex
+++ web/router.ex
@@ -13,10 +13,10 @@ defmodule Rumbl.Router do
   pipeline :api do
     plug :accepts, ["json"]
   end
-  
+
   scope "/manage", Rumbl do
     pipe_through [:browser, :authenticate_user]
-    
+
     resources "/videos", VideoController
   end
 
@@ -27,6 +27,7 @@ defmodule Rumbl.Router do
     resources "/users", UserController, only: [:index, :show, :new, :create]
     resources "/sessions", SessionController, only: [:new, :create, :delete]
     resources "/videos", VideoController
+    get "/watch/:id", WatchController, :show
   end
 
   # Other scopes may use custom stacks.
